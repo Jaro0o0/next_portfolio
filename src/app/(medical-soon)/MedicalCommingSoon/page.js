c419ba91a7f444d69223d2859538afe4fbc01e51
Jar0 <jar0dev80@gmail.com>
2026-05-15 16:51:31 +0200

    update-time

--- src/app/(medical-soon)/MedicalCommingSoon/page.js
+++ src/app/(medical-soon)/MedicalCommingSoon/page.js
@@ -4,7 +4,7 @@ import Countdown from "@/app/components/ComingSoon/Countdown";
 export default function CommingSoon() {
     // Definujemy datę docelową po stronie serwera
     // 30 dni od dzisiaj (piątek, 24 kwietnia 2026) -> 24 maja 2026
-    const targetDate = new Date("2026-05-15T00:00:00").getTime();
+    const targetDate = new Date("2026-05-18T00:00:00").getTime();
 
     return (
         <>
