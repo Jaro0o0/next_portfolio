31df82cb26e85ac7831e94e45242bbdec4f1c59f
Jaro0o0 <jar0dev80@gmail.com>
2026-08-17 23:49:07 +0200

    icons

--- src/lib/techStackData.js
+++ src/lib/techStackData.js
@@ -23,4 +23,7 @@ export const techItems = [
   { name: "Copilot AI", description: "AI Tool", src: "/icons/copilot.svg", className: "invert brightness-200", bgColor: "bg-white/10", borderColor: "hover:border-white/50", shadowColor: "group-hover:shadow-white/10" },
   { name: "C#", description: "Language", src: "/icons/c-sharp.png", className: "brightness-110 contrast-125 scale-110", bgColor: "bg-purple-400/10", borderColor: "hover:border-purple-400/50", shadowColor: "group-hover:shadow-white/10" },
   { name: "Net", description: "Runtime", src: "/icons/dotnet.svg", className: "brightness-110 contrast-125 scale-125", bgColor: "bg-violet-500/10", borderColor: "hover:border-violet-400/50", shadowColor: "group-hover:shadow-violet-400/10" },
+  { name: "Git", description: "Version Control", src: "/icons/git.svg", className: "brightness-110 contrast-125 scale-110", bgColor: "bg-orange-500/10", borderColor: "hover:border-orange-400/50", shadowColor: "group-hover:shadow-orange-400/10" },
+  
+  
 ];
